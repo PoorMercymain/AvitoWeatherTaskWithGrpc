0d51762c5b67916fcf41d851caa00ff7d8927a71
PoorMercymain <pinegin17@bk.ru>
2023-03-25 23:02:47 +0300

    first commit

---
+++ client/cmd/client.py
@@ -0,0 +1,90 @@
+import json
+
+from flask import Flask, request, jsonify
+import requests, datetime
+import dateutil.parser as datetime_parser
+from geopy.geocoders import Nominatim
+from os import getenv
+
+import grpc
+
+import auth_pb2, auth_pb2_grpc
+import auth_pb2_grpc
+
+app = Flask(__name__)
+
+geolocation = Nominatim(user_agent="SlyMercymain-tyap-lyap-app")
+
+
+@app.route("/v1/current/")
+def get_current_temperature():
+    city = request.args.get("city")
+    if city is None or city == "":
+        return jsonify({"Incorrect parameter": "City parameter is empty"})
+
+    coordinates = geolocation.geocode(city)
+    if coordinates is None:
+        return jsonify({"Incorrect parameter": "City parameter does not represent city name"})
+
+    reqStr = getenv("API") + "?latitude=" + str(coordinates.latitude) + "&longitude=" + str(coordinates.longitude) + "&current_weather=true"
+
+    if check_user():
+        response = requests.get(reqStr, timeout=20)
+        if response.status_code != 200:
+            return jsonify({"Incorrect request": "Something went wrong while getting response from weather API"})
+
+        json_response = response.json()
+
+        return jsonify({"city": city, "unit": "celsius", "temperature": json_response["current_weather"]["temperature"]})
+    return jsonify({"Incorrect username": "User not found in table"}), 403
+
+
+@app.route("/v1/forecast/")
+def get_forecast():
+    if request.args.get("dt") is None or request.args.get("dt") == "":
+        return jsonify({"Incorrect parameter": "Datetime parameter is empty"})
+
+    try:
+        dt = datetime_parser.parse(request.args.get("dt"))
+    except datetime_parser._parser.ParserError:
+        return jsonify({"Incorrect parameter": "Datetime parameter format is incorrerct. Try something like 2023-02-27T11:00"})
+
+    city = request.args.get("city")
+    if city is None or city == "":
+        return jsonify({"Incorrect parameter": "City parameter is empty"})
+
+    coordinates = geolocation.geocode(city)
+    if coordinates is None:
+        return jsonify({"Incorrect parameter": "City parameter does not represent city name"})
+
+    reqStr = getenv("API") + "?latitude=" + str(coordinates.latitude) + "&longitude=" + \
+             str(coordinates.longitude) + "&start_date=" + str(dt.date()) + "&end_date=" + str(dt.date()) +\
+             "&hourly=temperature_2m"
+
+    if check_user():
+        response = requests.get(reqStr, timeout=20)
+        if response.status_code != 200:
+            return jsonify({"Incorrect request": "Something went wrong while getting response from weather API"})
+
+        json_response = response.json()
+
+        return jsonify({"city": city, "unit": "celsius", "temperature": json_response["hourly"]["temperature_2m"][dt.hour], "time": dt})
+    return jsonify({"Incorrect username": "User not found in table"}), 403
+
+
+def check_user():
+    if request.headers["Own-Auth-UserName"] is None or request.headers["Own-Auth-UserName"] == "":
+        return jsonify({"Incorrect header": "Username header is empty"})
+
+    with grpc.insecure_channel('grpcserver:9000') as channel:
+        stub = auth_pb2_grpc.AuthStub(channel)
+
+        response = stub.CheckAuth(auth_pb2.AuthRequest(username=request.headers["Own-Auth-UserName"]))
+
+        if response.exists:
+            return True
+        return False
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=True, port=getenv("PORT"))
---
+++ server/cmd/server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"gRPC/pkg/api"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type GRPCServer struct {
+    api.AuthServer
+    users map[string]struct{}
+}
+
+func (s GRPCServer) CheckAuth(ctx context.Context, req *api.AuthRequest) (*api.AuthResponse, error) {
+    username := req.GetUsername()
+    fmt.Println(username)
+    _, ok := s.users[username]
+    if !ok {
+        return &api.AuthResponse{Exists: false}, nil
+    }
+
+    return &api.AuthResponse{Exists: true}, nil
+}
+
+func (s GRPCServer) mustEmbedUnimplementedAuthServer(ctx context.Context, req *api.AuthRequest) {}
+
+func main() {
+    s := grpc.NewServer()
+
+    users := make(map[string]struct{})
+    users["Echo"] = struct{}{}
+	users["Mercy"] = struct{}{}
+	users["D.Va"] = struct{}{}
+
+    srv := &GRPCServer{users: users}
+
+    api.RegisterAuthServer(s, srv)
+
+    l, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Сервер запущен")
+    fmt.Println(srv.users)
+
+	if err := s.Serve(l); err != nil {
+		log.Fatal(err)
+	}
+}
